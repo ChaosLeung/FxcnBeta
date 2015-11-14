38cefc681ccea673c93fccc90f47b82bc032aa0f
Chaos Leong <chaos@glassx.cn>
2015-11-14 16:54:11 +0800

    CnBetaUtil.java 添加 cb api 对应的简便调用方法

--- app/src/main/java/org/chaos/fx/cnbeta/net/CnBetaUtil.java
+++ app/src/main/java/org/chaos/fx/cnbeta/net/CnBetaUtil.java
@@ -1,12 +1,141 @@
 package org.chaos.fx.cnbeta.net;
 
+import org.chaos.fx.cnbeta.net.model.ArticleSummary;
+import org.chaos.fx.cnbeta.net.model.Comment;
+import org.chaos.fx.cnbeta.net.model.HotComment;
+import org.chaos.fx.cnbeta.net.model.NewsContent;
+import org.chaos.fx.cnbeta.net.model.Topic;
+
+import java.util.List;
+
+import retrofit.Call;
+
 /**
  * @author Chaos
  *         2015/11/03.
  */
 public class CnBetaUtil {
 
-    public static String getTypeString(int type){
+    public static String getTypeString(int type) {
         return type == CnBetaApi.TYPE_COMMENTS ? "comments" : type == CnBetaApi.TYPE_COUNTER ? "counter" : "dig";
+    }
+
+    public static Call<CnBetaApi.Result<List<ArticleSummary>>> articles(CnBetaApi api) {
+        long timestamp = System.currentTimeMillis();
+        return api.articles(timestamp, CnBetaSignUtil.articlesSign(timestamp));
+    }
+
+    Call<CnBetaApi.Result<List<ArticleSummary>>> topicArticles(CnBetaApi api, String topicId) {
+        long timestamp = System.currentTimeMillis();
+        return api.topicArticles(
+                timestamp,
+                CnBetaSignUtil.topicArticlesSign(timestamp, topicId),
+                topicId);
+    }
+
+    Call<CnBetaApi.Result<List<ArticleSummary>>> newArticles(CnBetaApi api, String topicId, String startSid) {
+        long timestamp = System.currentTimeMillis();
+        return api.newArticles(
+                timestamp,
+                CnBetaSignUtil.newArticlesSign(timestamp, topicId, startSid),
+                topicId,
+                startSid);
+    }
+
+    Call<CnBetaApi.Result<List<ArticleSummary>>> oldArticles(CnBetaApi api,
+                                                             String topicId,
+                                                             String endSid) {
+        long timestamp = System.currentTimeMillis();
+        return api.oldArticles(
+                timestamp,
+                CnBetaSignUtil.oldArticlesSign(timestamp, topicId, endSid),
+                topicId,
+                endSid);
+    }
+
+    Call<CnBetaApi.Result<NewsContent>> articleContent(CnBetaApi api,
+                                                       String sid) {
+        long timestamp = System.currentTimeMillis();
+        return api.articleContent(
+                timestamp,
+                CnBetaSignUtil.articleContentSign(timestamp, sid),
+                sid);
+    }
+
+    Call<CnBetaApi.Result<List<Comment>>> comments(CnBetaApi api,
+                                                   String sid,
+                                                   int page) {
+        long timestamp = System.currentTimeMillis();
+        return api.comments(
+                timestamp,
+                CnBetaSignUtil.commentsSign(timestamp, sid, page),
+                sid,
+                page);
+    }
+
+    Call<CnBetaApi.Result<Object>> addComment(CnBetaApi api,
+                                              String sid,
+                                              String content) {
+        long timestamp = System.currentTimeMillis();
+        return api.addComment(
+                timestamp,
+                CnBetaSignUtil.addCommentSign(timestamp, sid, content),
+                sid,
+                content);
+    }
+
+    Call<CnBetaApi.Result<Object>> replyComment(CnBetaApi api,
+                                                String sid,
+                                                String pid,
+                                                String content) {
+        long timestamp = System.currentTimeMillis();
+        return api.replyComment(
+                timestamp,
+                CnBetaSignUtil.replyCommentSign(timestamp, sid, pid, content),
+                sid,
+                pid,
+                content);
+    }
+
+    Call<CnBetaApi.Result<String>> supportComment(CnBetaApi api,
+                                                  String sid) {
+        long timestamp = System.currentTimeMillis();
+        return api.supportComment(
+                timestamp,
+                CnBetaSignUtil.supportCommentSign(timestamp, sid),
+                sid);
+    }
+
+    Call<CnBetaApi.Result<String>> againstComment(CnBetaApi api,
+                                                  String sid) {
+        long timestamp = System.currentTimeMillis();
+        return api.againstComment(
+                timestamp,
+                CnBetaSignUtil.againstCommentSign(timestamp, sid),
+                sid);
+    }
+
+    Call<CnBetaApi.Result<List<HotComment>>> hotComment(CnBetaApi api) {
+        long timestamp = System.currentTimeMillis();
+        return api.hotComment(timestamp, CnBetaSignUtil.hotCommentSign(timestamp));
+    }
+
+    Call<CnBetaApi.Result<List<ArticleSummary>>> todayRank(CnBetaApi api,
+                                                           String type) {
+        long timestamp = System.currentTimeMillis();
+        return api.todayRank(
+                timestamp,
+                CnBetaSignUtil.todayRankSign(timestamp, type),
+                type);
+    }
+
+    Call<CnBetaApi.Result<List<ArticleSummary>>> top10(CnBetaApi api) {
+        long timestamp = System.currentTimeMillis();
+        return api.top10(timestamp, CnBetaSignUtil.top10Sign(timestamp));
+    }
+
+    Call<CnBetaApi.Result<List<Topic>>> topics(CnBetaApi api) {
+        long timestamp = System.currentTimeMillis();
+        return api.topics(timestamp, CnBetaSignUtil.topicsSign(timestamp));
     }
 }

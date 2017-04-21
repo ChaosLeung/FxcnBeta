6419bde09abf3e669a7609993bbe474f9717943f
Chaos Leong <lgf42031@gmail.com>
2017-04-21 22:11:57 +0800

    [Feature] Remove 'Daily Rank'

--- app/src/main/java/org/chaos/fx/cnbeta/MainActivity.java
+++ app/src/main/java/org/chaos/fx/cnbeta/MainActivity.java
@@ -16,12 +16,10 @@
 
 package org.chaos.fx.cnbeta;
 
-import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -32,7 +30,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.animation.DecelerateInterpolator;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabReselectListener;
@@ -44,7 +41,6 @@ import org.chaos.fx.cnbeta.hotcomment.HotCommentFragment;
 import org.chaos.fx.cnbeta.preferences.PreferenceHelper;
 import org.chaos.fx.cnbeta.preferences.PreferenceKeys;
 import org.chaos.fx.cnbeta.preferences.PreferencesActivity;
-import org.chaos.fx.cnbeta.rank.RanksFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,33 +48,11 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener, ReselectedDispatcher {
 
-    private static final int DURATION_EXPAND_TAB_LAYOUT = 300;
-    private static final int DURATION_COLLAPSE_TAB_LAYOUT = 200;
-
     @BindView(R.id.toolbar) Toolbar mToolbar;
-    @BindView(R.id.tabs) TabLayout mTabLayout;
     @BindView(R.id.bottom_bar) BottomBar mBottomBar;
     @BindView(R.id.pager) ViewPager mViewPager;
 
     private String[] mPageTitles;
-
-    private ValueAnimator mExpandAnimator;
-    private ValueAnimator mCollapseAnimator;
-
-    private ValueAnimator.AnimatorUpdateListener mTabLayoutAnimatorListener = new ValueAnimator.AnimatorUpdateListener() {
-        @Override
-        public void onAnimationUpdate(ValueAnimator animation) {
-            float alpha = 1;
-            if (animation == mExpandAnimator) {
-                alpha = animation.getAnimatedFraction();
-            } else if (animation == mCollapseAnimator) {
-                alpha = 1 - animation.getAnimatedFraction();
-            }
-            mTabLayout.setAlpha(alpha);
-            mTabLayout.getLayoutParams().height = (int) animation.getAnimatedValue();
-            mTabLayout.requestLayout();
-        }
-    };
 
     private SharedPreferences mDefaultPreferences;
 
@@ -93,24 +67,16 @@ public class MainActivity extends AppCompatActivity implements
 
         setSupportActionBar(mToolbar);
 
-        mPageTitles = new String[]{getString(R.string.nav_home), getString(R.string.nav_rank),
+        mPageTitles = new String[]{getString(R.string.nav_home),
                 getString(R.string.nav_hot_articles), getString(R.string.nav_hot_comments)};
         mOnReselectListeners = new SparseArray<>(mPageTitles.length);
 
-        setTitle(mPageTitles[0]);
         mViewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
-        mViewPager.setOffscreenPageLimit(4);
+        mViewPager.setOffscreenPageLimit(mPageTitles.length);
         mViewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
-                if (position == 1) {// RankFragment
-                    expandTabLayout();
-                } else if (mTabLayout.getHeight() != 0) {
-                    collapseTabLayout();
-                }
-
                 mBottomBar.selectTabAtPosition(position, true);
-                setTitle(mPageTitles[position]);
             }
         });
         mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
@@ -137,43 +103,6 @@ public class MainActivity extends AppCompatActivity implements
     protected void onDestroy() {
         super.onDestroy();
         mDefaultPreferences.unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    private void expandTabLayout() {
-        if (mCollapseAnimator != null && mCollapseAnimator.isRunning()) {
-            mCollapseAnimator.cancel();
-        }
-
-        int prevHeight = mTabLayout.getHeight();
-        int targetHeight = getResources().getDimensionPixelSize(R.dimen.tab_layout_height);
-
-        if (mExpandAnimator == null) {
-            mExpandAnimator = ValueAnimator.ofInt(prevHeight, targetHeight);
-            mExpandAnimator.setInterpolator(new DecelerateInterpolator());
-            mExpandAnimator.setDuration(DURATION_EXPAND_TAB_LAYOUT);
-            mExpandAnimator.addUpdateListener(mTabLayoutAnimatorListener);
-        } else {
-            mExpandAnimator.setIntValues(prevHeight, targetHeight);
-        }
-        mExpandAnimator.start();
-    }
-
-    private void collapseTabLayout() {
-        if (mExpandAnimator != null && mExpandAnimator.isRunning()) {
-            mExpandAnimator.cancel();
-        }
-
-        int prevHeight = mTabLayout.getHeight();
-        int targetHeight = 0;
-        if (mCollapseAnimator == null) {
-            mCollapseAnimator = ValueAnimator.ofInt(prevHeight, targetHeight);
-            mCollapseAnimator.setInterpolator(new DecelerateInterpolator());
-            mCollapseAnimator.setDuration(DURATION_COLLAPSE_TAB_LAYOUT);
-            mCollapseAnimator.addUpdateListener(mTabLayoutAnimatorListener);
-        } else {
-            mCollapseAnimator.setIntValues(prevHeight, targetHeight);
-        }
-        mCollapseAnimator.start();
     }
 
     @Override
@@ -225,10 +154,8 @@ public class MainActivity extends AppCompatActivity implements
                 case 0:
                     return ArticlesFragment.newInstance("null");
                 case 1:
-                    return RanksFragment.newInstance();
-                case 2:
                     return Top10Fragment.newInstance();
-                case 3:
+                case 2:
                     return HotCommentFragment.newInstance();
             }
             return new Fragment();
